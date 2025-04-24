17c41590ffc505388b86bbfa183da152cebd623f
Evortex <codex03@mail.ru>
2025-04-24 22:10:54 +0700

    Реализация редактирование данных товара, удаление данных товара, обработка ошибок через веб-интерфейс для модуля Manager App

--- manager-app/src/main/java/com/example/managerapp/controller/ProductsController.java
+++ manager-app/src/main/java/com/example/managerapp/controller/ProductsController.java
@@ -6,10 +6,10 @@ import com.example.managerapp.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,15 +30,15 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(NewProductPayload payload) {
-
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/list/%d".formatted(product.getId());
-    }
-
-    @GetMapping("{product:\\d+}")
-    public String getProduct(@PathVariable("product") int productId, Model model) {
-        model.addAttribute("product", this.productService.findProduct(productId).orElseThrow());
-        return "catalogue/products/product";
+    public String createProduct(@Validated NewProductPayload payload, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+            .map(ObjectError::getDefaultMessage).toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/list/%d".formatted(product.getId());
+        }
     }
 }
--- manager-app/src/main/java/com/example/managerapp/service/ProductService.java
+++ manager-app/src/main/java/com/example/managerapp/service/ProductService.java
@@ -12,6 +12,10 @@ public interface ProductService {
 
     Product createProduct(String title, String details);
 
-
     Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+
 }
